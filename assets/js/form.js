54bc03f8349f9fe68fd5fad4145d1353a1b65913
AMakowski <adrianlmr1994@gmail.com>
2021-09-08 22:58:31 -0300

    Entrega SELECTORES Y EVENTOS

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,72 +1,23 @@
+$(document).ready( ()=>{
+  $("#buttonConfirmar").on("click" , captura);
+});
 
-let botonAgregar = document.querySelector('#btnAgregarEdu');
-function agregarEducacion(){
-  if (!document.getElementById("educaciondiv")){
-    let divEducacion = document.createElement("div");
-    divEducacion.id = "educaciondiv"
-    divEducacion.innerHTML = `  <div class="row"> 
-                                <div class="form-group col-md-6">
-                                <label for="Institucion">Institucion: </label>
-                                <input type="text" class="form-control" id="Institucion" >
-                                </div>
-                                <div class="form-group col-md-6">
-                                <label for="name">Años: </label>
-                                <input type="text" class="form-control" id="ciudad" >
-                                </div>
-                                </div>`
-    document.getElementById("contenedorEducacion").appendChild(divEducacion);
-  }
-}
-botonAgregar.addEventListener('click', agregarEducacion);
-
-let educacion = [
-  {
-      institucion: document.getElementById('institucion').value,
-      anios: document.getElementById('anios').value,
-  }
-]
-if(document.getElementById('educaciondiv')){
-  let educacion2 = {
-      institucion: document.getElementById('institucion2').value,
-      anios: document.getElementById('anios2').value,
-  }
-  educacion.push(educacion2);
-}
-localStorage.setItem('edu', educacion);
-
-
+///////////boton confirmar////////////
 function captura() {
-  let nombre = document.getElementById("nombre").value;
-  let apellido= document.getElementById("apellido").value;
-  let nacimiento = document.getElementById("nacimiento").value;
-  let edad = document.getElementById("edad").value;
-  let telefono = document.getElementById("telefono").value;
-  let ciudad = document.getElementById("ciudad").value;
-  let pais = document.getElementById("pais").value;
-  let email = document.getElementById("email").value;
-  let cargo = document.getElementById("cargo").value;
-  let descripcion = document.getElementById("descripcion").value;
-  let resumen = document.getElementById("resumen").value;
-  let twitter = document.getElementById("twitter").value;
-  let instagram = document.getElementById("instagram").value;
-  let github = document.getElementById("github").value;
-  let linkedin = document.getElementById("linkedin").value;
-  localStorage.setItem('nombre' , nombre);
-  localStorage.setItem('apellido' , apellido);
-  localStorage.setItem('nacimiento' , nacimiento);
-  localStorage.setItem('edad' , edad);
-  localStorage.setItem('telefono' , telefono);
-  localStorage.setItem('ciudad' , ciudad);
-  localStorage.setItem('pais' , pais);
-  localStorage.setItem('email' , email);
-  localStorage.setItem('cargo' , cargo);
-  localStorage.setItem('descripcion' , descripcion);
-  localStorage.setItem('resumen' , resumen)
-  localStorage.setItem('twitter' , twitter)
-  localStorage.setItem('instagram' , instagram)
-  localStorage.setItem('github' , github)
-  localStorage.setItem('linkedin' , linkedin)
+  localStorage.setItem('nombre' , $("#nombre").val());
+  localStorage.setItem('apellido' , $("#apellido").val());
+  localStorage.setItem('nacimiento' , $("#nacimiento").val());
+  localStorage.setItem('edad' , $("#edad").val());
+  localStorage.setItem('telefono' , $("#telefono").val());
+  localStorage.setItem('ciudad' , $("#ciudad").val());
+  localStorage.setItem('pais' , $("#pais").val());
+  localStorage.setItem('email' , $("#email").val());
+  localStorage.setItem('cargo' , $("#cargo").val());
+  localStorage.setItem('descripcion' , $("#descripcion").val());
+  localStorage.setItem('resumen' , $("#resumen").val())
+  localStorage.setItem('twitter' , $("#twitter").val())
+  localStorage.setItem('instagram' , $("#instagram").val())
+  localStorage.setItem('github' , $("#github").val())
+  localStorage.setItem('linkedin' , $("#linkedin").val())
   window.location="portfolio.html";
 }
-
-
