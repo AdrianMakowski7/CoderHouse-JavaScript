9c1a734feacc24794190a456f933a9275138596e
AMakowski <adrianlmr1994@gmail.com>
2021-10-05 23:37:31 -0300

    entrega final

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,5 +1,9 @@
+let codigoEducacion = 0;
+let codigoTrabajo = 0;
+let arrayTrabajos = [];
+let arrayEducacion= [];
+
 $(document).ready( ()=>{
-  $("#buttonConfirmar").on("click" , captura);
   $("#nombre").val(localStorage.getItem('nombre'));
   $("#apellido").val(localStorage.getItem('apellido'));
   $("#nacimiento").val(localStorage.getItem('nacimiento'));
@@ -8,54 +12,155 @@ $(document).ready( ()=>{
   $("#ciudad").val(localStorage.getItem('ciudad'));
   $("#pais").val(localStorage.getItem('pais'));
   $("#email").val(localStorage.getItem('email'));
-  $("#cargo").val(localStorage.getItem('cargo'));
+  $("#competencia").val(localStorage.getItem('competencia'));
   $("#descripcion").val(localStorage.getItem('descripcion'));
   $("#resumen").val(localStorage.getItem('resumen'));
   $("#twitter").val(localStorage.getItem('twitter'));
   $("#instagram").val(localStorage.getItem('instagram'));
   $("#github").val(localStorage.getItem('github'));
   $("#linkedin").val(localStorage.getItem('linkedin'));
-  $(`#btnAgregarEdu`).on("click" , agregar);
-  
-  
-  
+  $(`#btnAgregarEdu`).on("click" , agregarEducacion);
+  $(`#btnAgregarExp`).on("click" , agregarTrabajos);
 });
 
-
-function agregar(){
+function agregarEducacion(){
   let institucion = $("#institucion").val();
   let inicioInst = $("#inicioInst").val();
   let finInst = $("#finInst").val();
   let descripcionInst = $("#descripcionInst").val();
-  $("#tablaInst").append(`<tr class="eduAnimacion"><td> ${institucion} </td><td> ${inicioInst} </td><td> ${finInst} </td><td> ${descripcionInst} </td><td class="text-center"><input value=" X " type="button" class="btnMenos"></td></tr>`);
-  $(".btnMenos").on("click", borrar);
+  codigoEducacion = codigoEducacion+1; 
+  $("#tablaInst").append(`<tr class="eduAnimacion" id="${codigoEducacion}"><td> ${institucion} </td><td> ${inicioInst} </td><td> ${finInst} </td><td> ${descripcionInst} 
+  </td><td class="text-center"><input value=" X " type="button" class="btnMenos" codigoeducacion="${codigoEducacion}"></td></tr>`);
+  $(".btnMenos").on("click", borrarEdu);
+  let objetoEdu = { 
+    "institucion": institucion, 
+    "inicio": inicioInst,
+    "fin": finInst,
+    "descripcion": descripcionInst
+  };
+  arrayEducacion[codigoEducacion]=objetoEdu;
 };
 
-function borrar(){
-$(this).parent().parent().remove();
-}  
+function borrarEdu(){
+  let codigoEdu = $(this).attr(`codigoeducacion`);
+  $(`#${codigoEdu}`).remove();
+  localStorage.removeItem(codigoEdu);
+}
 
+function agregarTrabajos(){
+  let empresaExp = $("#empresaExp").val();
+  let inicioExp = $("#inicioExp").val();
+  let finExp = $("#finExp").val();
+  let descripcionExp = $("#descripcionExp").val();
+  codigoTrabajo = codigoTrabajo+1
+  $("#tablaExp").append(`<tr class="eduAnimacion" codigo"${codigoTrabajo}"><td> ${empresaExp} </td><td> ${inicioExp} </td><td> ${finExp} </td><td> ${descripcionExp} 
+  </td><td class="text-center"><input value=" X " type="button" class="btnMenos1" codigotrabajo="${codigoTrabajo}"></td></tr>`);
+  $(".btnMenos1").on("click", borrarTrab);
+  let objetoTrab = { 
+    "empresa": empresaExp, 
+    "inicio": inicioExp,
+    "fin": finExp,
+    "descripcion": descripcionExp
+  };
+  arrayTrabajos[codigoTrabajo]=objetoTrab;
+};
 
-  
-  
-  ///////////boton confirmar////////////
-  function captura() {
-    localStorage.setItem('nombre' , $("#nombre").val());
-    localStorage.setItem('apellido' , $("#apellido").val());
-    localStorage.setItem('nacimiento' , $("#nacimiento").val());
-    localStorage.setItem('edad' , $("#edad").val());
-    localStorage.setItem('telefono' , $("#telefono").val());
-    localStorage.setItem('ciudad' , $("#ciudad").val());
-    localStorage.setItem('pais' , $("#pais").val());
-    localStorage.setItem('email' , $("#email").val());
-    localStorage.setItem('cargo' , $("#cargo").val());
-    localStorage.setItem('descripcion' , $("#descripcion").val());
-    localStorage.setItem('resumen' , $("#resumen").val())
-    localStorage.setItem('twitter' , $("#twitter").val())
-    localStorage.setItem('instagram' , $("#instagram").val())
-    localStorage.setItem('github' , $("#github").val())
-    localStorage.setItem('linkedin' , $("#linkedin").val())
-    window.location="portfolio.html";
-  }
-  
-  
+function borrarTrab(){
+  let codigoTrab = $(this).attr(`codigoTrabajo`);
+  $(`#${codigoTrab}`).remove();
+  localStorage.removeItem(codigoTrab);
+}
+/////////////////////Validacion Formularop/////////////////////////
+let formulario = document.getElementById('formularyx');
+let input = document.querySelectorAll('#formularyx input');
+
+const expresiones = {
+  expNombre: /^[a-zA-ZÀ-ÿ\s]{4,16}$/,
+	expCorreo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	expEdad: /^\d{1,2}$/,
+	expTelefono: /^\d{7,14}$/,
+  expEmpleos: /^[a-zA-ZÀ-ÿ\s,]{4,50}$/
+}
+
+let camposValidacion = {
+  nombre : false,
+  apellido : false,
+  edad : false,
+  ciudad : false,
+  pais : false,
+  competencia : false,
+  email : false,
+  telefono : false,
+}
+
+let validarFormulario = (e) => {
+  switch (e.target.id) {
+    case "nombre":
+      validacionDeCampo(expresiones.expNombre, e.target, e.target.id)
+      break;
+      case "apellido":
+        validacionDeCampo(expresiones.expNombre, e.target, e.target.id)
+        break;
+        case "edad":
+          validacionDeCampo(expresiones.expEdad, e.target, e.target.id)
+          break;
+          case "ciudad":
+            validacionDeCampo(expresiones.expNombre, e.target, e.target.id)
+            break;
+            case "pais":
+              validacionDeCampo(expresiones.expNombre, e.target, e.target.id)
+              break;
+              case "competencia":
+                validacionDeCampo(expresiones.expEmpleos, e.target, e.target.id)
+                break;
+                case "email":
+                  validacionDeCampo(expresiones.expCorreo, e.target, e.target.id)
+                  break;
+                  case "telefono":
+                    validacionDeCampo(expresiones.expTelefono, e.target, e.target.id)
+                    break;
+                  }
+                }
+                
+                let validacionDeCampo = (expresiones, input, campo) => {
+                  if(expresiones.test(input.value)){
+                    console.log("entro")
+                    document.getElementById(`error${campo}`).classList.remove(`erroresActivos`)
+                    camposValidacion[campo]= true
+                  }else{
+                    document.getElementById(`error${campo}`).classList.add(`erroresActivos`)
+                    camposValidacion[campo]= false
+                  }
+                }
+                
+                input.forEach((input) => {
+                  input.addEventListener('keyup', validarFormulario);
+                  input.addEventListener('blur', validarFormulario);
+                });
+                
+                formulario.addEventListener('submit', (e) =>{
+                  e.preventDefault();
+                  if(camposValidacion.nombre && camposValidacion.apellido && camposValidacion.edad && camposValidacion.ciudad && camposValidacion.pais && camposValidacion.competencia && camposValidacion.email && camposValidacion.telefono){
+                    localStorage.setItem('nombre' , $("#nombre").val());
+                    localStorage.setItem('apellido' , $("#apellido").val());
+                    localStorage.setItem('nacimiento' , $("#nacimiento").val());
+                    localStorage.setItem('edad' , $("#edad").val());
+                    localStorage.setItem('telefono' , $("#telefono").val());
+                    localStorage.setItem('ciudad' , $("#ciudad").val());
+                    localStorage.setItem('pais' , $("#pais").val());
+                    localStorage.setItem('email' , $("#email").val());
+                    localStorage.setItem('competencia' , $("#competencia").val());
+                    localStorage.setItem('descripcion' , $("#descripcion").val());
+                    localStorage.setItem('resumen' , $("#resumen").val());
+                    localStorage.setItem('twitter' , $("#twitter").val());
+                    localStorage.setItem('instagram' , $("#instagram").val());
+                    localStorage.setItem('github' , $("#github").val());
+                    localStorage.setItem('linkedin' , $("#linkedin").val());
+                    localStorage.setItem("arrayTrabajos",JSON.stringify(arrayTrabajos));
+                    localStorage.setItem("arrayEducacion",JSON.stringify(arrayEducacion));
+                    window.location="portfolio.html";
+                  }else{
+                    document.getElementById(`errormensaje`).classList.add(`erroresActivos`)
+                  }
+                });
+                ///////////////////////////Fin validacion////////////////////////////
--- assets/js/port.js
+++ assets/js/port.js
@@ -1,4 +1,5 @@
 $(document).ready(()=>{
+  comprobaciones();
   let mostarNombre = localStorage.getItem("nombre");
   let mostarApellido = localStorage.getItem("apellido");
   $(".nombre1").html(`${mostarNombre} ${mostarApellido}`);
@@ -17,6 +18,8 @@ $(document).ready(()=>{
   $("#descripcion1").html(`${mostarDescripcion}`);
   let mostarResumen = localStorage.getItem("resumen");
   $("#resumen1").html(`${mostarResumen}`);
+  let mostarCompetencia = localStorage.getItem("competencia");
+  $(".competencia1").html(`${mostarCompetencia}`);
   let mostarTwitter = localStorage.getItem("twitter");
   $("#twitter1").attr(`href`, mostarTwitter);
   let mostarInstagram = localStorage.getItem("instagram");
@@ -25,17 +28,55 @@ $(document).ready(()=>{
   $("#github1").attr(`href`, mostarGithub);
   let mostarLinkedin = localStorage.getItem("linkedin");
   $("#linkedin1").attr(`href`, mostarLinkedin);
-  
-  
-/*  $("#btnEnviar").click(() =>{
-    $.post(linkUrl, datosCont, (estado)=>{
-      if(estado === "success"){
-        alert("mensaje enviado")
-      }
-    })
-  });
+});
+
+let comprobaciones = () =>{
+  if(localStorage.getItem("twitter") == ""){
+    document.getElementById(`twitter1`).classList.add(`ocultar`)
+  }
+  if(localStorage.getItem("instagram") == ""){
+    document.getElementById(`instagram1`).classList.add(`ocultar`)
+  }
+  if(localStorage.getItem("linkedin") == ""){
+    document.getElementById(`linkedin1`).classList.add(`ocultar`)
+  }
+  if(localStorage.getItem("github") == ""){
+    document.getElementById(`github1`).classList.add(`ocultar`)
+  }
+  let compe = document.getElementById(`cartel`)
+  compe.setAttribute(`data-typed-items` , `${localStorage.getItem("competencia")}`)
+
+  let typed_strings = localStorage.getItem("competencia");
+      typed_strings = typed_strings.split(',')
+      new Typed('.typed', {
+        strings: typed_strings,
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2000
+});
+ 
+};
+var trabajosArray = localStorage.getItem("arrayTrabajos")
+var arrayTrab = JSON.parse(trabajosArray)
+
+console.log(arrayTrab)
+      
+var educacionArray = localStorage.getItem("arrayEducacion")
+var arrayEduc = JSON.parse(educacionArray)
+/*
+arrayEduc.map((ed)=>{
+  console.log(ed)
+
+  if(!ed.institucion){
+    return;
+  }else{
+    document.getElementById(`divEducacion`).append(
+      "<div class='resume-item'>"+
+      "<h4>"+ed.institucion+"</h4>"+
+      "<h5>"+ed.inicio+" - "+ ed.fin+"</h5>"+
+      "<p>"+ed.descripcion+"</p>"+
+      "</div>"
+      )
+    }
 })*/
-
-
-const linkUrl = "https://jsonplaceholder.typicode.com/posts"
-const datosCont = {nombreC: $("#nameContacto").val() , emailC: $("#emailContacto").val(), motivoContacto: $("#motivoContacto").val(),mensajeContacto: $("#mensajeContacto").val()}
